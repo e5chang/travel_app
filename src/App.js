e5c9135f2aa230aa288afdbfbc717df7aa82ee9d
Ethan Chang <eschang120@gmail.com>
2022-10-19 15:00:08 -0700

    first commit - button & navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
